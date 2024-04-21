6b89d308613bdf8e1923ff97ab9938cdcb84a48f
Hermann <douanlahermann93@gmailcom>
2024-04-21 21:32:12 +0100

    ok

--- src/Pages/Download.tsx
+++ src/Pages/Download.tsx
@@ -1,11 +1,7 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import iPhone from "../assets/img/portrait_black.png";
-import appStore from "../assets/img/app-store-badge.svg";
-import playStore from "../assets/img/google-play-badge.svg";
-import { Link } from "react-router-dom";
-import Details from "../components/Detail";
+import Details from "../components/Details";
 
 const Download = () => {
   return (
