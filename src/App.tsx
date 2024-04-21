921a53ef869217653f336d3d5b7b70627e9dc541
Hermann <douanlahermann93@gmailcom>
2024-04-21 22:00:04 +0100

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.class';
 import React from 'react'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Home from './Pages/Home'
-import NotFound from './Pages/NotFound';
-import Download from './Pages/Download';
-import Contact from './Pages/Contact';
-import About from './Pages/About';
-import Marketplace from './Pages/Marketplace';
 
-const routes = createBrowserRouter([
-  {
-    path: "/",
-    Component: Home,
-    errorElement: <NotFound />,
-  },
-  {
-    path: "/download",
-    Component: Download,
-    errorElement: <NotFound />
-  },
-  {
-    path: "/contact",
-    Component: Contact,
-    errorElement: <NotFound />
-  },
-  {
-    path: "/about",
-    Component: About,
-    errorElement: <NotFound />
-  },
-  {
-    path: "/marketplace",
-    Component: Marketplace,
-    errorElement: <NotFound />,
-  }
-]);
-
-export default function App() {
+const App = () => {
   return (
-    <RouterProvider router={routes} />
+    <div>
+      <p>Hello world</p>
+    </div>
   )
 }
+
+export default App
