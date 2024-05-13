d4e093dd258225c1d723de74360954fcf58bd4c2
Hermann2710 <138356589+Hermann2710@users.noreply.github.com>
2024-05-13 15:01:13 +0100

    Modification

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -1,11 +1,16 @@
 const people = [
   {
-    name: "Leslie Alexander",
-    role: "Co-Founder / CEO",
+    name: "Abel MK",
+    role: "Designer",
     imageUrl:
       "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
   },
-  // More people...
+  {
+    name: "Douanla Hermann",
+    role: "Designer",
+    imageUrl:
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+  },
 ];
 
 export default function Team() {
